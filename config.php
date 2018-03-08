7e44e263b63ace97217baab249df0167f41dd7e4
Dhawal Agrawal <dhawal55555@gmail.com>
2018-03-08 11:54:53 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 $mysql_hostname = "localhost";
 $mysql_user     = "root";
 $mysql_password = "";
-$mysql_database = "codeclub";
+$mysql_database = "myOnlineCV";
 
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 mysql_select_db($mysql_database, $bd) or die("Could not select database");
