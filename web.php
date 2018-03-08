514a34a57df7a945340bef12b9b001c714216162
Dhawal Agrawal <dhawal55555@gmail.com>
2018-03-08 11:10:32 +0530

    Added web version.

---
+++ web.php
@@ -0,0 +1,249 @@
+<?php include('config.php'); ?>
+
+<!-----------------------Header Start--------------------->
+<div id="head">
+<div id="logo"><img src="IMAGES/mylogo.png" alt="TEMPLOGO" style="height: 45px; margin: 9px 0; float: left;"/>
+<div id="logoname">Dhawal</div>
+</div>
+</div>
+<!-----------------------Header End----------------------->
+
+<div id="container"  data-PageSwitch>
+        <div class="sections">
+            <div class="section" id="section0">
+                <div class="bo" id="bo0" >
+                <div id="c0">
+                <div id="c0-L">
+                <div id="h2"><span class="main-letter">A</span>bout me</div>
+          		<div id="c0-L-info"><img src="IMAGES/22045937_1322627981197169_4115721413201365603_n.jpg"  alt="my pic" /></div>
+                </div>
+                <div id="c0-R"><h1>Dhawal Agrawal</h1>
+                <h2> CODER &nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;WEB DEVELOPER&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;ENTREPRENEUR </h2>
+                <p>I'm a sophomore at HBTU-Kanpur. Being an IT student, I have a keen interest in Competitive Programming, Web Development, Ethical Hacking and Machine Learning. I have a passion for web design and love to create for web and mobile devices. I'm also the founder and president of <a href="http://decoder.co.in" target="_blank">DECODER</a> (coding club of our university). The picture you see on the left is of one of the seminars I organised as a part of the club.</br></br> Besides coding, I love travelling, cooking, playing badminton and also exploring new Aerospace Technology.</p>
+                <div id="c0-R-info"><a href="https://www.visualcv.com/dhawal-agrawal/pdf" target="_blank" download>DOWNLOAD RESUME&nbsp;&nbsp;<i class="fas fa-download"></i></a>
+                </div>
+                </div>
+                </div>
+                </div>
+                
+          	</div>
+            
+            
+            
+            <div class="section" id="section1">
+                <div class="bo" id="bo1" >
+                <div id="c1">
+                <div id="c1-L">
+                <div id="h2"><span class="main-letter">P</span>ortfolio</div>
+          		<div id="c1-L-info"><div id="buttons"></div></div>
+                </div>
+                <div id="c1-R"><div id="gallery">
+  
+      <a href="http://hbtumail.decoder.co.in/" target="_blank"><img src="IMAGES/HBTUmail.jpg" data-tags="Projects" alt="Alt 1" /><span class="tooltiptext">Developed Webmail service of our college</span></a>
+  
+      <a href="https://www.facebook.com/mnnitecell/" target="_blank"><img src="IMAGES/Ecellmnnit.jpg" data-tags="Work experience" alt="Alt 2" /><span class="tooltiptext">Campus Ambassador</br>E-Cell</br>MNNIT Allahabad</span></a>
+
+        <a href="http://dhawalagrawal.decoder.co.in/IMAGES/youngindia.jpg" target="_blank"><img src="IMAGES/youngindia.jpg" data-tags="Achievements" alt="Alt 3" /><span class="tooltiptext">1st Prize</br>City Final & School Final</br>Young India Challenge</span></a>
+
+        <a href="http://digikolorz.com/" target="_blank"><img src="IMAGES/DIGIKOLORZ.jpg" data-tags="Work experience" alt="Alt 4" /><span class="tooltiptext">Trainee</br>Web Development</br>Digikolorz</span></a>
+        <a href="http://decoder.co.in/" target="_blank"><img src="IMAGES/Decoder.jpg" data-tags="Projects,Work experience" alt="Alt 5" /><span class="tooltiptext">Made Decoder Website</br>Work as Founder and President at Decoder</span></a>
+
+        <a href="http://dhawalagrawal.decoder.co.in/IMAGES/technomatica.jpg" target="_blank"><img src="IMAGES/technomatica.jpg" data-tags="Achievements" alt="Alt 6" /><span class="tooltiptext">1st Prize</br>Coding Wizard Competition</br>Technomatica,St. Peter's Agra</br></span></a>
+
+        <a href="http://dhawalagrawal.decoder.co.in/" target="_blank"><img src="IMAGES/mylogo.jpg" data-tags="Projects" alt="Alt 7" /><span class="tooltiptext">Developed My Online CV</span></a>
+        <a href="http://hbtu.ac.in" target="_blank"><img src="IMAGES/HBTUKanpur.jpg" data-tags="Education" alt="Alt 8" /><span class="tooltiptext">2nd B.Tech. I.T.</br>HBTU Kanpur</span></a>
+
+        <a href="http://dhawalagrawal.decoder.co.in/IMAGES/cms.jpg" target="_blank"><img src="IMAGES/cms.jpg" data-tags="Achievements" alt="Alt 9" /><span class="tooltiptext">3rd prize</br>Cover Page Design Competition</br> CMS Lucknow</span></a>
+        
+
+
+    </div></div>
+                </div>
+                
+          	</div>
+            </div>
+            
+            
+            <div class="section" id="section2">
+                <div class="bo" id="bo2" >
+                <div id="c2">
+                <div id="c2-L">
+                <div id="h2"><span class="main-letter">S</span>kills</div>
+          		<div id="c2-L-info">
+                
+     <div class="p">PHOTOSHOP</div>
+    <div id="bar1" class="barfiller">
+     <div class="tipWrap">
+		<span class="tip"></span>
+	    </div>
+	    <span class="fill" data-percentage="85"></span>
+	</div>
+	
+    
+    <div class="p">HTML5 / CSS3</div>
+	<div id="bar2" class="barfiller">
+	    <div class="tipWrap">
+		<span class="tip"></span>
+	    </div>
+	    <span class="fill" data-percentage="98"></span>
+	</div>	
+	
+	
+	<div class="p">DEVELOPMENT</div>
+	<div id="bar3" class="barfiller">
+	    <div class="tipWrap">
+		<span class="tip"></span>
+	    </div>
+	    <span class="fill" data-percentage="80"></span>
+	</div>	
+
+	
+	<div class="p">PHP / MYSQL</div>
+	<div id="bar4" class="barfiller">
+	    <div class="tipWrap">
+		<span class="tip"></span>
+	    </div>
+	    <span class="fill" data-percentage="90"></span>
+	</div>	
+	
+	
+    <div class="p">PYTHON / DJANGO</div>
+    <div id="bar5" class="barfiller">
+	    <div class="tipWrap">
+		<span class="tip"></span>
+	    </div>
+	    <span class="fill" data-percentage="75"></span>
+	</div>	
+	
+	
+    <div class="p">COMPETITIVE PROGRAMMING</div>
+    <div id="bar6" class="barfiller">
+	    <div class="tipWrap">
+		<span class="tip"></span>
+	    </div>
+	    <span class="fill" data-percentage="78"></span>
+	</div>	
+	
+	
+    <div class="p">REACT JS</div>
+    <div id="bar7" class="barfiller">
+	    <div class="tipWrap">
+		<span class="tip"></span>
+	    </div>
+	    <span class="fill" data-percentage="79"></span>
+	</div>	
+	
+	
+    
+    </div>
+                </div>
+                <div id="c2-R">
+                <div id="h2"><span class="main-letter">T</span>estimonials</div>
+                <div id="c2-R-info"><div class="wrapper">
+        <div class="gallery">
+        	<div class="gallery__img-block  ">
+                <span class="">
+                    Varun Khare
+                </span>
+                <img src="IMAGES/testimonial2.jpg" thumb-url="IMAGES/testimonial2.jpg" class="">
+            </div>
+            <div class="gallery__img-block  ">
+                <span class="">
+                    Zaki Rayeen
+                </span>
+                <img src="IMAGES/testimonial 1.jpg" thumb-url="IMAGES/testimonial 1.jpg" class="">
+            </div>
+            
+             
+             
+            <div class="gallery__controls">
+
+            </div>
+        </div>
+    </div>
+    </div>
+                </div>
+                </div>
+                </div>
+            </div>
+            
+            
+          	<div class="section" id="section3">
+                <div class="bo" id="bo3" >
+                <div id="c3">
+                <div id="c3-L">
+                <div id="h2"><span class="main-letter">B</span>logs</div>
+          		<div id="c3-L-info"><a href="http://www.decoder.co.in/blog/" target="_blank">View All</a>
+                </div>
+                </div>
+                <div id="c3-R">
+                <div id="c3-R-info"><a href="" target="_blank"><img src="IMAGES/web-design-2906159_1920.jpg" alt="b1" /></a><div id="c3-R-info-da">09 February 2018</div><div id="c3-R-info-t"><a href="" target="_blank">Step-wise guide on How to begin with Coding?</a></div><div id="c3-R-info-au">by Dhawal Agrawal</div></div>
+                <div id="c3-R-info"><a href="" target="_blank"><img src="IMAGES/hacking-2903156_1920.jpg" alt="b1" /></a><div id="c3-R-info-da">10 May 2018</div><div id="c3-R-info-t"><a href="" target="_blank">A Simple Way To Learn Cyber-Security.</a></div><div id="c3-R-info-au">by Dhawal Agrawal</div></div>
+                <div id="c3-R-info"><a href="" target="_blank"><img src="IMAGES/entrepreneur-593372_1920.jpg" alt="b1" /></a><div id="c3-R-info-da">12 December 2018</div><div id="c3-R-info-t"><a href="" target="_blank">8 Ways That Blogging Will Kickstart Your Startup.</a></div><div id="c3-R-info-au">by Dhawal Agrawal</div></div>
+                
+                </div>
+                </div>
+                
+          	</div></div>
+          
+            <div class="section" id="section4">
+                <div class="bo" id="bo4" >
+                <div id="c4">
+                <div id="c4-L">
+                <div id="h1"><span class="main-letter">C</span>ontact</div>
+          		<div id="c4-L-info">
+                <div id="c4-L-info-a"><div id="c4-L-info-i"><i class="fab fa-telegram-plane"></i></div>
+                <div id="c4-L-info-d"><a href="https://gmail.com" target="_blank">dhawal55555@gamil.com</a></div></div>
+                <div id="c4-L-info-a"><div id="c4-L-info-i"><i class="fas fa-phone-square"></i></div>
+                <div id="c4-L-info-d"><a> +91-9456675497 , +91-7007976855</a></div></div>
+                <div id="c4-L-info-a"><div id="c4-L-info-i"><i class="fas fa-home"></i></div>
+                <div id="c4-L-info-d" style="    padding: 7px 0 0 25px;"><a href="https://goo.gl/maps/xZbaxgJVy232" target="_blank">A-47, Indrapuri, Agra-5,</br>Uttar Pradesh 285005</a></div></div>
+                <div id="c4-L-info-a">
+                <div id="c4-L-info-s"><a href="https://github.com/DhawalAgrawal" target="_blank"><i class="fab fa-github"></i></a></div>
+                <div id="c4-L-info-s"><a href="https://www.facebook.com/dhawal.agrawal.10" target="_blank"><i class="fab fa-facebook"></i></a></div>
+                <div id="c4-L-info-s"><a href="https://www.hackerrank.com/coding_wizard" target="_blank"><img src="IMAGES/HACKER.png" alt="HACKER" width="35px" /></a></div>
+                </div>
+                </div>
+                </div>
+                <div id="c4-R">
+                <form action="submit.php" method="post">
+                <span>
+                <input name="name" type="text" placeholder="Name:" required />
+                <input name="email" type="email" placeholder="Email:" required />
+                <textarea name="message" cols="" rows="" placeholder="Message:" required ></textarea>
+                </span>
+                <input name="submit" id="submit" type="submit" value="Submit" />
+                </form></div>
+				</div></div>
+            </div>
+        </div>
+    </div>
+    
+<script src="js/page.js"></script>
+    <script>
+        $("#container").PageSwitch({
+            direction: "horizontal",
+        })
+    </script>
+    
+    <script src="js/jquery.barfiller.js" type="text/javascript"></script>
+<script type="text/javascript">
+
+$(document).ready(function(){
+
+	$('#bar1').barfiller({ barColor: '#f9cc4a', duration: 5000 });
+	$('#bar2').barfiller({ barColor: '#f9cc4a', duration: 5000 });
+	$('#bar3').barfiller({ barColor: '#f9cc4a', duration: 5000 });
+	$('#bar4').barfiller({ barColor: '#f9cc4a', duration: 5000 });
+	$('#bar5').barfiller({ barColor: '#f9cc4a', duration: 5000 });
+	$('#bar6').barfiller({ barColor: '#f9cc4a', duration: 5000 });
+	$('#bar7').barfiller({ barColor: '#f9cc4a', duration: 5000 });
+	
+});
+
+</script>
+
+
+<script src="js/filter-tags.js"></script>
+<script src="js/vit-gallery.js"></script>
+<script src="js/main.js"></script>
